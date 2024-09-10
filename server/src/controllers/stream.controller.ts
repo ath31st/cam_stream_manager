db59389dcffd20243f4d9dcda906c19c65764922
ath31st <ath31st@yandex.ru>
2024-09-10 09:57:22 +0300

    add stream controller

---
+++ server/src/controllers/stream.controller.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from 'express';
+import { StreamService } from '../services/stream.service';
+import { NewStreamDto, UpdateStreamDto } from '../utils/types';
+
+export class StreamController {
+  private streamService: StreamService;
+
+  constructor(streamService: StreamService) {
+    this.streamService = streamService;
+  }
+
+  createStream = async (req: Request, res: Response) => {
+    try {
+      const dto: NewStreamDto = req.body;
+      await this.streamService.createStream(dto);
+      res.status(201).json({ message: 'Stream created successfully' });
+    } catch (error) {
+      if (error instanceof Error) {
+        res
+          .status(500)
+          .json({ message: 'Failed to create stream', error: error.message });
+      } else {
+        res.status(500).json({
+          message: 'Failed to create stream',
+          error: 'Unknown error occurred',
+        });
+      }
+    }
+  };
+
+  updateStream = async (req: Request, res: Response) => {
+    try {
+      const dto: UpdateStreamDto = { id: Number(req.params.id), ...req.body };
+      await this.streamService.updateStream(dto);
+      res.status(200).json({ message: 'Stream updated successfully' });
+    } catch (error) {
+      if (error instanceof Error) {
+        res
+          .status(500)
+          .json({ message: 'Failed to update stream', error: error.message });
+      } else {
+        res.status(500).json({
+          message: 'Failed to update stream',
+          error: 'Unknown error occurred',
+        });
+      }
+    }
+  };
+
+  deleteStream = async (req: Request, res: Response) => {
+    try {
+      const streamId = Number(req.params.id);
+      await this.streamService.deleteStream(streamId);
+      res.status(200).json({ message: 'Stream deleted successfully' });
+    } catch (error) {
+      if (error instanceof Error) {
+        res
+          .status(500)
+          .json({ message: 'Failed to delete stream', error: error.message });
+      } else {
+        res.status(500).json({
+          message: 'Failed to delete stream',
+          error: 'Unknown error occurred',
+        });
+      }
+    }
+  };
+
+  getAllStreams = async (req: Request, res: Response) => {
+    try {
+      const streams = await this.streamService.getAllStreams();
+      res.status(200).json(streams);
+    } catch (error) {
+      if (error instanceof Error) {
+        res.status(500).json({
+          message: 'Failed to retrieve streams',
+          error: error.message,
+        });
+      } else {
+        res.status(500).json({
+          message: 'Failed to retrieve streams',
+          error: 'Unknown error occurred',
+        });
+      }
+    }
+  };
+}
