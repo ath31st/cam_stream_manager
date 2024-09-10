296f636a2d11d97ad74aa549cf9fb746e121bd43
ath31st <ath31st@yandex.ru>
2024-09-10 15:14:30 +0300

    clean stream routes

--- server/src/routes/stream.routes.ts
+++ server/src/routes/stream.routes.ts
@@ -1,15 +1,6 @@
 import { Router } from 'express';
-import { StreamService } from '../services/stream.service';
-import { StreamRepository } from '../repositories/stream.repository';
-import { StreamController } from '../controllers/stream.controller';
-import { prismaService } from '../database/prisma.service';
+import { streamController } from '../utils/init';
 
-const prisma = prismaService;
-const streamRepository = new StreamRepository(prisma.client);
-const streamService = new StreamService(streamRepository);
-const streamController = new StreamController(streamService);
-
-const apiPrefix = '/api/v1';
 const router = Router();
 
 router.post('/streams', streamController.createStream);
@@ -19,6 +10,7 @@ router.get('/streams', streamController.getAllStreams);
 router.get('/streams/:id', streamController.getStream);
 
 const streamRoutes = Router();
+const apiPrefix = '/api/v1';
 streamRoutes.use(apiPrefix, router);
 
 export default streamRoutes;
