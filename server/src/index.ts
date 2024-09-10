832ab07e6c52c712fcd5a9e4994c0b1d07983789
ath31st <ath31st@yandex.ru>
2024-09-10 11:11:33 +0300

    update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,22 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import streamRoutes from './routes/stream.routes';
 
 dotenv.config();
 
 const app: Express = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(streamRoutes);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
