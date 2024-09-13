cc190203642262609d6d122e9110071e2aa69c2f
ath31st <ath31st@yandex.ru>
2024-09-13 13:33:03 +0300

    update init.ts with services, controllers, repositories

--- server/src/utils/init.ts
+++ server/src/utils/init.ts
@@ -2,10 +2,25 @@ import { StreamService } from '../services/stream.service';
 import { StreamRepository } from '../repositories/stream.repository';
 import { StreamController } from '../controllers/stream.controller';
 import { prismaService } from '../database/prisma.service';
+import { ResponsiblePersonRepository } from '../repositories/responsible.person.repository';
+import { ResponsiblePersonService } from '../services/responsible.person.service';
+import { RegionRepository } from '../repositories/region.repository';
+import { RegionService } from '../services/region.service';
+import { RegionController } from '../controllers/region.controller';
+import { ResponsiblePersonController } from '../controllers/responsible.person.controller';
 
 const prisma = prismaService;
+
+const rpRepository = new ResponsiblePersonRepository(prisma.client);
+const rpService = new ResponsiblePersonService(rpRepository);
+const rpController = new ResponsiblePersonController(rpService);
+
+const regionRepository = new RegionRepository(prisma.client);
+const regionService = new RegionService(regionRepository);
+const regionController = new RegionController(regionService);
+
 const streamRepository = new StreamRepository(prisma.client);
 const streamService = new StreamService(streamRepository);
-const streamController = new StreamController(streamService);
+const streamController = new StreamController(streamService, rpService);
 
-export { streamController, streamService };
+export { streamController, streamService, regionController, rpController };
