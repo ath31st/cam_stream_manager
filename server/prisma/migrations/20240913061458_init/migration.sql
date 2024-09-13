e3abe3c681646b57e1f9818db9ba4d9b7f152d08
ath31st <ath31st@yandex.ru>
2024-09-13 09:15:52 +0300

    update prisma models, change releations between tables

--- server/prisma/migrations/20240913061458_init/migration.sql
+++ server/prisma/migrations/20240913061458_init/migration.sql
@@ -12,8 +12,10 @@ CREATE TABLE "ResponsiblePerson" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
+    "streamId" INTEGER NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "ResponsiblePerson_streamId_fkey" FOREIGN KEY ("streamId") REFERENCES "Stream" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateTable
@@ -29,17 +31,3 @@ CREATE TABLE "Stream" (
     "updatedAt" DATETIME NOT NULL,
     CONSTRAINT "Stream_regionId_fkey" FOREIGN KEY ("regionId") REFERENCES "Region" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
-
--- CreateTable
-CREATE TABLE "_ResponsiblePersonToStream" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL,
-    CONSTRAINT "_ResponsiblePersonToStream_A_fkey" FOREIGN KEY ("A") REFERENCES "ResponsiblePerson" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT "_ResponsiblePersonToStream_B_fkey" FOREIGN KEY ("B") REFERENCES "Stream" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "_ResponsiblePersonToStream_AB_unique" ON "_ResponsiblePersonToStream"("A", "B");
-
--- CreateIndex
-CREATE INDEX "_ResponsiblePersonToStream_B_index" ON "_ResponsiblePersonToStream"("B");
