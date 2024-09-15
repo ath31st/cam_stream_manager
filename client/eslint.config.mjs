df692e608f8f2654a8590a761f8c0c0bde07e9e4
ath31st <ath31st@yandex.ru>
2024-09-15 12:39:29 +0300

    fixes config eslint and package.json

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -4,6 +4,7 @@ import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
 
 export default [
+  { settings: { react: { version: 'detect' } } },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
