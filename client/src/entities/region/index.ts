d9257abf5e04010737aec18e0b12f41e19efad85
ath31st <ath31st@yandex.ru>
2024-09-18 14:21:41 +0300

    update index.ts for region entity

---
+++ client/src/entities/region/index.ts
@@ -0,0 +1,5 @@
+import { RegionDto, NewRegionDto, UpdateRegionDto } from '@shared/types';
+
+export type Region = RegionDto;
+export type NewRegion = NewRegionDto;
+export type UpdateRegion = UpdateRegionDto;
