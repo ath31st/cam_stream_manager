5d3275fd858a6918f11ddabc7d71b986e2c69eb9
ath31st <ath31st@yandex.ru>
2024-09-18 14:59:42 +0300

    import resp.person types from shared

---
+++ client/src/entities/responsible.person/index.ts
@@ -0,0 +1,9 @@
+import {
+  ResponsiblePersonDto,
+  NewResponsiblePersonDto,
+  UpdateResponsiblePersonDto,
+} from '@shared/types';
+
+export type ResponsiblePerson = ResponsiblePersonDto;
+export type NewResponsiblePerson = NewResponsiblePersonDto;
+export type UpdateResponsiblePerson = UpdateResponsiblePersonDto;
