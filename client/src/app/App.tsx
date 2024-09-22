0c054d875613141e853a308bfc7a096bc456aca8
ath31st <ath31st@yandex.ru>
2024-09-22 10:34:09 +0300

    update app tsx

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import AppRoutes from './routers/AppRoutes';
 
 function App() {
-  return <div className="App">Test</div>;
+  return <AppRoutes />;
 }
 
 export default App;
