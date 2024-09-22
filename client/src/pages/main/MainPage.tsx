d0b5e0c8ddaf08af3ca74af0a2079e0abdcc53ee
ath31st <ath31st@yandex.ru>
2024-09-22 10:33:58 +0300

    add simple main page

---
+++ client/src/pages/main/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div style={{ padding: '24px' }}>
+      <h1>Добро пожаловать на главную страницу!</h1>
+      <p>Здесь будет основное содержимое для пользователей.</p>
+    </div>
+  );
+};
+
+export default MainPage;
