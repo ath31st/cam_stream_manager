9c610ca5b2d3072c7e66b581293e7f2b18202761
ath31st <ath31st@yandex.ru>
2024-09-23 11:10:54 +0300

    rework sider component

--- client/src/widgets/sider/ui/Sider.tsx
+++ client/src/widgets/sider/ui/Sider.tsx
@@ -1,45 +1,88 @@
 import React, { useEffect, useState } from 'react';
-import { Layout, Menu, Spin } from 'antd';
+import { Layout, Menu, MenuProps } from 'antd';
 import { useRegionStore } from '../../../app/stores/region.store';
 import { useStreamStore } from '../../../app/stores/stream.store';
 import styles from './Sider.module.css';
 
 const { Sider } = Layout;
-const { SubMenu } = Menu;
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+interface LevelKeysProps {
+  key?: string;
+  children?: LevelKeysProps[];
+}
+
+const getLevelKeys = (items: LevelKeysProps[]) => {
+  const key: Record<string, number> = {};
+  const func = (items: LevelKeysProps[], level = 1) => {
+    items.forEach((item) => {
+      if (item.key) {
+        key[item.key] = level;
+      }
+      if (item.children) {
+        func(item.children, level + 1);
+      }
+    });
+  };
+  func(items);
+  return key;
+};
 
 export const AppSider: React.FC = () => {
   const { regions, fetchAllRegions } = useRegionStore();
-  const { streams, fetchStreamsByRegion, loading } = useStreamStore();
-  const [selectedRegionId, setSelectedRegionId] = useState<number | null>(null);
+  const { streams, fetchStreamsByRegion } = useStreamStore();
+  const [stateOpenKeys, setStateOpenKeys] = useState<string[]>(['0']);
 
   useEffect(() => {
     fetchAllRegions();
   }, [fetchAllRegions]);
 
   const handleRegionClick = (regionId: number) => {
-    setSelectedRegionId(regionId);
     fetchStreamsByRegion(regionId);
+  };
+
+  const items: MenuItem[] = regions.map((region) => ({
+    key: String(region.id),
+    label: region.name,
+    onTitleClick: () => handleRegionClick(region.id),
+    children: streams
+      .filter((stream) => stream.regionId === region.id)
+      .map((stream) => ({
+        key: `${region.id}-${stream.id}`,
+        label: stream.location,
+      })),
+  }));
+
+  const levelKeys = getLevelKeys(items as LevelKeysProps[]);
+
+  const onOpenChange: MenuProps['onOpenChange'] = (openKeys) => {
+    const currentOpenKey = openKeys.find(
+      (key) => stateOpenKeys.indexOf(key) === -1,
+    );
+    if (currentOpenKey !== undefined) {
+      const repeatIndex = openKeys
+        .filter((key) => key !== currentOpenKey)
+        .findIndex((key) => levelKeys[key] === levelKeys[currentOpenKey]);
+
+      setStateOpenKeys(
+        openKeys
+          .filter((_, index) => index !== repeatIndex)
+          .filter((key) => levelKeys[key] <= levelKeys[currentOpenKey]),
+      );
+    } else {
+      setStateOpenKeys(openKeys);
+    }
   };
 
   return (
     <Sider className={styles['site-layout-background']}>
-      <Menu mode="inline" className={styles.menu}>
-        {regions.map((region) => (
-          <SubMenu
-            key={region.id}
-            title={region.name}
-            onTitleClick={() => handleRegionClick(region.id)}
-          >
-            {loading && selectedRegionId === region.id ? (
-              <Spin />
-            ) : (
-              streams?.map((stream) => (
-                <Menu.Item key={stream.id}>{stream.location}</Menu.Item>
-              ))
-            )}
-          </SubMenu>
-        ))}
-      </Menu>
+      <Menu
+        mode="inline"
+        openKeys={stateOpenKeys}
+        onOpenChange={onOpenChange}
+        items={items}
+      />
     </Sider>
   );
 };
