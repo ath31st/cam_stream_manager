6b4e04ed70df4e133be123ab66452d28e22e6176
ath31st <ath31st@yandex.ru>
2024-09-23 13:34:42 +0300

    add player widget

---
+++ client/src/widgets/player/ui/Player.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useStreamStore } from '../../../app/stores/stream.store';
+import HLSPlayer from '../../../shared';
+import styles from './Player.module.css';
+
+export const Player: React.FC = () => {
+  const { selectedStream } = useStreamStore();
+
+  return (
+    <div className={styles['player-container']}>
+      {selectedStream ? (
+        selectedStream.status === 'Active' ? (
+          <HLSPlayer url={selectedStream.streamUrl} />
+        ) : (
+          <div className="placeholder">Стрим недоступен</div>
+        )
+      ) : (
+        <div className="placeholder">Выберите стрим из списка</div>
+      )}
+    </div>
+  );
+};
