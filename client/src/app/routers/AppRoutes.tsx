c41847d1af47c0ebf0a81bb403a45441cfebfafb
ath31st <ath31st@yandex.ru>
2024-09-26 19:36:08 +0300

    update app routes with admin page

--- client/src/app/routers/AppRoutes.tsx
+++ client/src/app/routers/AppRoutes.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainPage from '../../pages/main';
 import MainLayout from '../layouts/MainLayout';
+import AdminPage from '../../pages/admin';
+import AdminLayout from '../layouts/AdminLayout';
 
 const AppRoutes: React.FC = () => {
   return (
     <Router>
-      <MainLayout>
-        <Routes>
+      <Routes>
+        <Route element={<MainLayout />}>
           <Route path="/" element={<MainPage />} />
-        </Routes>
-      </MainLayout>
+        </Route>
+        <Route element={<AdminLayout />}>
+          <Route path="/admin" element={<AdminPage />} />
+        </Route>
+      </Routes>
     </Router>
   );
 };
