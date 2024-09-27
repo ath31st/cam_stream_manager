fa8e9833cf7055d6e035010d8399c585216fd5ee
ath31st <ath31st@yandex.ru>
2024-09-27 11:03:11 +0300

    update region entity index

--- client/src/entities/region/index.ts
+++ client/src/entities/region/index.ts
@@ -1,5 +1,9 @@
 import { RegionDto, NewRegionDto, UpdateRegionDto } from '@shared/types';
+import AddRegionModal from './ui/AddRegionModal';
+import DeleteRegionModal from './ui/DeleteRegionModal';
 
 export type Region = RegionDto;
 export type NewRegion = NewRegionDto;
 export type UpdateRegion = UpdateRegionDto;
+
+export { AddRegionModal, DeleteRegionModal };
