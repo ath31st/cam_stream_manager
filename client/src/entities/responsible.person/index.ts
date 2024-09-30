012a57115c7157f23d11645cdaba2f8fc3735364
ath31st <ath31st@yandex.ru>
2024-09-30 11:49:59 +0300

    update index responsible person entity

--- client/src/entities/responsible.person/index.ts
+++ client/src/entities/responsible.person/index.ts
@@ -3,7 +3,16 @@ import {
   NewResponsiblePersonDto,
   UpdateResponsiblePersonDto,
 } from '@shared/types';
+import DeleteResponsiblePersonModal from './ui/DeleteResponsiblePersonModal';
+import AddResponsiblePersonModal from './ui/AddResponsiblePersonModal';
+import UpdateResponsiblePersonModal from './ui/UpdateResponsiblePersonModal';
 
 export type ResponsiblePerson = ResponsiblePersonDto;
 export type NewResponsiblePerson = NewResponsiblePersonDto;
 export type UpdateResponsiblePerson = UpdateResponsiblePersonDto;
+
+export {
+  DeleteResponsiblePersonModal,
+  AddResponsiblePersonModal,
+  UpdateResponsiblePersonModal,
+};
