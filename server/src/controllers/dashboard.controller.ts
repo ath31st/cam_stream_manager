dc3f084094147daa8102d805031afbeaebe8c7aa
ath31st <ath31st@yandex.ru>
2024-10-04 10:26:40 +0300

    add dashboard controller

---
+++ server/src/controllers/dashboard.controller.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { DashboardService } from '../services/dashboard.service';
+
+export class DashboardController {
+  private dashboardService: DashboardService;
+
+  constructor(dashboardService: DashboardService) {
+    this.dashboardService = dashboardService;
+  }
+
+  getDashboardData = async (req: Request, res: Response) => {
+    try {
+      const dashboardData = await this.dashboardService.getDashboardData();
+      res.status(200).json(dashboardData);
+    } catch (error) {
+      if (error instanceof Error) {
+        res
+          .status(400)
+          .json({
+            message: 'Failed to get dashboard data',
+            error: error.message,
+          });
+      } else {
+        res.status(500).json({
+          message: 'Failed to get dashboard data',
+          error: 'Unknown error occurred',
+        });
+      }
+    }
+  };
+}
