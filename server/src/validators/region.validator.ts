a87be6eacafc373e15661d4e80e8313b6389cfa3
ath31st <ath31st@yandex.ru>
2024-10-05 10:54:45 +0300

    add region validator

---
+++ server/src/validators/region.validator.ts
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+import { NewRegionDto, UpdateRegionDto } from '@shared/types';
+
+export const newRegionSchema = Joi.object<NewRegionDto>({
+  name: Joi.string().min(2).max(30).required(),
+});
+
+export const updateRegionSchema = Joi.object<UpdateRegionDto>({
+  id: Joi.number().integer().required(),
+  name: Joi.string().min(2).max(30).optional(),
+  isVisible: Joi.boolean().optional(),
+});
