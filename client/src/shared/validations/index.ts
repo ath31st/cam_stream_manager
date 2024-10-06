5d6aea497f9b307950776d4e59463ba1ad9b8153
ath31st <ath31st@yandex.ru>
2024-10-06 16:38:48 +0300

    add validation rules functions and index.ts

---
+++ client/src/shared/validations/index.ts
@@ -0,0 +1,17 @@
+import {
+  phoneValidationRules,
+  rpNameValidationRules,
+  streamCommentValidationRules,
+  streamLocationValidationRules,
+  streamUrlValidationRules,
+  regionNameValidationRules,
+} from './lib/validation.rules';
+
+export {
+  phoneValidationRules,
+  rpNameValidationRules,
+  streamCommentValidationRules,
+  streamLocationValidationRules,
+  streamUrlValidationRules,
+  regionNameValidationRules,
+};
---
+++ client/src/shared/validations/lib/validation.rules.ts
@@ -0,0 +1,49 @@
+import {
+  phoneErrorMessage,
+  phonePattern,
+  urlErrorMessage,
+  urlPattern,
+} from '../constants/validation.constants';
+
+const getLengthRule = (min: number, max: number) => ({
+  min,
+  max,
+  message: `Длина должна быть от ${min} до ${max} символов`,
+});
+
+export const streamLocationValidationRules = [
+  { required: true, message: 'Введите местоположение потока' },
+  getLengthRule(3, 100),
+];
+
+export const streamUrlValidationRules = [
+  { required: true, message: 'Введите URL потока' },
+  getLengthRule(1, 1000),
+  {
+    pattern: urlPattern,
+    message: urlErrorMessage,
+  },
+];
+
+export const streamCommentValidationRules = [
+  { required: false, message: 'Комментарии (необязательно)' },
+  getLengthRule(0, 500),
+];
+
+export const regionNameValidationRules = [
+  { required: true, message: 'Введите название региона' },
+  getLengthRule(2, 30),
+];
+
+export const rpNameValidationRules = [
+  { required: true, message: 'Введите ответственное лицо' },
+  getLengthRule(2, 100),
+];
+
+export const phoneValidationRules = [
+  { required: true, message: 'Введите номер телефона' },
+  {
+    pattern: phonePattern,
+    message: phoneErrorMessage,
+  },
+];
