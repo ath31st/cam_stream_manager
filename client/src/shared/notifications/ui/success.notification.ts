0f33c2f7beea1c71645df2d3d9663dfb87fe68bf
ath31st <ath31st@yandex.ru>
2024-10-07 11:43:11 +0300

    fix success notification

--- client/src/shared/notifications/ui/success.notification.ts
+++ client/src/shared/notifications/ui/success.notification.ts
@@ -1,16 +1,11 @@
 import { notification } from 'antd';
 import { notificationDurations } from '../constants/notification.constants';
 
-export const successNotification = (
-  message: string,
-  onClose: () => void,
-  description?: string,
-) => {
+export const successNotification = (message: string, description?: string) => {
   notification.success({
     message,
     description,
     placement: 'topRight',
     duration: notificationDurations.success,
-    onClose,
   });
 };
