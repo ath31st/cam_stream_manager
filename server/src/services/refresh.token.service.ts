c87187a540834880d4474c7f1109984549c7f68e
ath31st <ath31st@yandex.ru>
2024-10-11 11:10:15 +0300

    add refresh token service

---
+++ server/src/services/refresh.token.service.ts
@@ -0,0 +1,45 @@
+import { RefreshTokenRepository } from '../repositories/refresh.token.repository';
+
+export class RefreshTokenService {
+  private refreshTokenRepository: RefreshTokenRepository;
+
+  constructor(refreshTokenRepository: RefreshTokenRepository) {
+    this.refreshTokenRepository = refreshTokenRepository;
+  }
+
+  async saveRefreshToken(
+    userId: number,
+    refreshToken: string,
+    expiresAt: Date,
+  ): Promise<void> {
+    await this.refreshTokenRepository.create(refreshToken, userId, expiresAt);
+  }
+
+  async getRefreshToken(token: string) {
+    return await this.refreshTokenRepository.getByToken(token);
+  }
+
+  async deleteRefreshTokenByToken(token: string): Promise<void> {
+    await this.refreshTokenRepository.deleteByToken(token);
+  }
+
+  async deleteRefreshTokenByUserId(userId: number): Promise<void> {
+    await this.refreshTokenRepository.deleteByUserId(userId);
+  }
+
+  async deleteExpiredTokens(): Promise<void> {
+    await this.refreshTokenRepository.deleteExpiredTokens();
+  }
+
+  async updateRefreshToken(
+    oldToken: string,
+    newToken: string,
+    newExpiresAt: Date,
+  ): Promise<void> {
+    await this.refreshTokenRepository.updateToken(
+      oldToken,
+      newToken,
+      newExpiresAt,
+    );
+  }
+}
