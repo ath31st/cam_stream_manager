b1d268d6d1d52d5811fdda1558e87ca1bab29f1a
ath31st <ath31st@yandex.ru>
2024-10-12 10:30:20 +0300

    fix auth api login method

--- client/src/features/auth/api/auth.api.ts
+++ client/src/features/auth/api/auth.api.ts
@@ -4,9 +4,7 @@ import { Login, Tokens } from '../index';
 const API_URL = process.env.REACT_APP_API_URL;
 
 export const login = async (loginData: Login): Promise<Tokens> => {
-  const response = await axios.post(`${API_URL}/login`, {
-    loginData,
-  });
+  const response = await axios.post(`${API_URL}/login`, loginData);
   return response.data;
 };
 
