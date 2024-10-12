0c2c8e23ce5300a4ceab2b9550b994c779c8c035
ath31st <ath31st@yandex.ru>
2024-10-12 10:43:53 +0300

    update header with auth module

--- client/src/widgets/header/ui/Header.tsx
+++ client/src/widgets/header/ui/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout } from 'antd';
 import styles from './Header.module.css';
+import { AuthModule } from '../../../features/auth';
 
 const { Header } = Layout;
 
@@ -8,6 +9,8 @@ export const AppHeader: React.FC = () => {
   return (
     <Header className={styles.header}>
       <div className={styles.logo} />
+
+      <AuthModule />
     </Header>
   );
 };
