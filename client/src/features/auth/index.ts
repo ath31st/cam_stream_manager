66effd3fd370dca9429aa39f8c0259eafa379d19
ath31st <ath31st@yandex.ru>
2024-10-12 09:36:37 +0300

    add jwt lib

--- client/src/features/auth/index.ts
+++ client/src/features/auth/index.ts
@@ -1,6 +1,4 @@
 import { LoginDto, TokensDto } from '@shared/types';
-import { login, logout, refreshAccessToken } from './api/auth.api';
 
 export type Login = LoginDto;
 export type Tokens = TokensDto;
-export { login, logout, refreshAccessToken };
