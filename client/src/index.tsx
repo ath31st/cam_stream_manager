e9fb6379ac9ca5c4b1e7454f553cacb3859f01d5
ath31st <ath31st@yandex.ru>
2024-10-15 13:41:06 +0300

    add provider in app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,20 +6,23 @@ import LocaleProvider from './app/providers/LocaleProvider';
 import AxiosInterceptorProvider from './app/providers/AxiosInterceptorProvider';
 import { ServerStatusOverlay } from './features/server.status';
 import AuthProvider from './app/providers/AuthProvider';
+import GlobalStyleProvider from './app/providers/GlobalStyleProvider';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <LocaleProvider>
-      <AxiosInterceptorProvider>
-        <AuthProvider>
-          <ServerStatusOverlay />
-          <App />
-        </AuthProvider>
-      </AxiosInterceptorProvider>
-    </LocaleProvider>
+    <GlobalStyleProvider>
+      <LocaleProvider>
+        <AxiosInterceptorProvider>
+          <AuthProvider>
+            <ServerStatusOverlay />
+            <App />
+          </AuthProvider>
+        </AxiosInterceptorProvider>
+      </LocaleProvider>
+    </GlobalStyleProvider>
   </React.StrictMode>,
 );
 
