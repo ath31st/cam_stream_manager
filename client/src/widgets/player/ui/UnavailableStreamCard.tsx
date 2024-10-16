9b7fdd68ce4aa0617981d11756ed5d7e46fd18bd
ath31st <ath31st@yandex.ru>
2024-10-16 09:28:37 +0300

    add unavailable stream card for player

---
+++ client/src/widgets/player/ui/UnavailableStreamCard.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card } from 'antd';
+import { WarningOutlined } from '@ant-design/icons';
+import styles from './UnavailableStreamCard.module.css';
+import { ResponsiblePerson } from '../../../entities/responsible.person';
+
+interface UnavailableStreamCardProps {
+  responsiblePersons: ResponsiblePerson[];
+}
+
+const UnavailableStreamCard: React.FC<UnavailableStreamCardProps> = ({
+  responsiblePersons,
+}) => (
+  <Card className={styles['unavailable-card']}>
+    <WarningOutlined className={styles['icon']} />
+    <h2>Поток временно недоступен</h2>
+    <p>Обратитесь по телефону к ответственным лицам:</p>
+    <ul className={styles['responsible-list']}>
+      {responsiblePersons.map((person) => (
+        <li key={person.id}>
+          {person.name}: <strong>{person.phone}</strong>
+        </li>
+      ))}
+    </ul>
+  </Card>
+);
+
+export default UnavailableStreamCard;
