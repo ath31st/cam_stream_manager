2fa638464ad9d914571bd343ca2c4d8ebf7a930f
ath31st <ath31st@yandex.ru>
2024-10-16 10:49:22 +0300

    update player and styles

--- client/src/widgets/player/ui/Player.tsx
+++ client/src/widgets/player/ui/Player.tsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useStreamStore } from '../../../app/stores/stream.store';
 import { HLSPlayer } from '../../../shared/hls.player';
 import styles from './Player.module.css';
 import { useResponsiblePersonStore } from '../../../app/stores/responsible.person.store';
-import ResponsiblePersons from './ResponsiblePersons';
+import UnavailableStreamCard from './UnavailableStreamCard';
+import { ResponsiblePersonModal } from '../../../entities/responsible.person';
+import WelcomeCard from './WelcomeCard';
 
 export const Player: React.FC = () => {
   const { selectedStream } = useStreamStore();
   const { fetchResponsiblePersonsByStream, responsiblePersons } =
     useResponsiblePersonStore();
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
     if (selectedStream) {
@@ -16,19 +20,35 @@ export const Player: React.FC = () => {
     }
   }, [selectedStream, fetchResponsiblePersonsByStream]);
 
+  const openModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setIsModalVisible(false);
+  };
+
   return (
     <div className={styles['player-container']}>
       {selectedStream ? (
         <>
           {selectedStream.status === 'Active' ? (
-            <HLSPlayer url={selectedStream.streamUrl} />
+            <>
+              <HLSPlayer url={selectedStream.streamUrl} />
+              <button onClick={openModal}>Показать ответственных лиц</button>
+            </>
           ) : (
-            <div className="placeholder">Стрим недоступен</div>
+            <UnavailableStreamCard responsiblePersons={responsiblePersons} />
           )}
-          <ResponsiblePersons responsiblePersons={responsiblePersons} />
+
+          <ResponsiblePersonModal
+            onClose={closeModal}
+            isOpen={isModalVisible}
+            responsiblePersons={responsiblePersons}
+          />
         </>
       ) : (
-        <div className="placeholder">Выберите стрим из списка</div>
+        <WelcomeCard />
       )}
     </div>
   );
