4e20d3e9c96c3f4ca29a48eca4df27fbcaa7fbaf
ath31st <ath31st@yandex.ru>
2024-10-18 10:58:48 +0300

    add region search selector component

---
+++ client/src/shared/ui/selects/RegionSelect.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Select } from 'antd';
+import { Region } from '../../../entities/region';
+import styles from './RegionSelect.module.css';
+
+interface RegionSelectProps {
+  regions: Region[];
+  value?: number;
+  onChange?: (value: number | null) => void;
+  placeholder?: string;
+}
+
+const RegionSelect: React.FC<RegionSelectProps> = ({
+  regions,
+  value,
+  onChange,
+  placeholder = 'Выберите регион',
+}) => {
+  return (
+    <Select
+      className={styles['search-select-field']}
+      showSearch
+      allowClear
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      optionFilterProp="children"
+      filterOption={(input, option) =>
+        (option?.children as unknown as string)
+          .toLowerCase()
+          .includes(input.toLowerCase())
+      }
+    >
+      {regions.map((region) => (
+        <Select.Option key={region.id} value={region.id}>
+          {region.name}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+export default RegionSelect;
