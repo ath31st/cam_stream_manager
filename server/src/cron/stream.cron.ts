b93cef3f2671483c31b5582041aa6e8d467e943d
ath31st <ath31st@yandex.ru>
2024-11-06 19:12:12 +0300

    add change period cron job for server

--- server/src/cron/stream.cron.ts
+++ server/src/cron/stream.cron.ts
@@ -3,8 +3,17 @@ import { streamService } from '../utils/init';
 import { Logger } from '../utils/logger';
 
 export const startStreamCronJob = () => {
-  cron.schedule('*/1 * * * *', () => {
-    Logger.log('Running scheduled task to update stream statuses...');
+  const cronSchedule = process.env.CRON_SCHEDULE;
+
+  if (!cronSchedule) {
+    Logger.log('CRON_SCHEDULE is not set. Scheduled task will not run.');
+    return;
+  }
+
+  cron.schedule(cronSchedule, () => {
+    Logger.log(
+      `Running scheduled task to update stream statuses with cron schedule: ${cronSchedule}...`,
+    );
     streamService.pingAllStreams();
   });
 };
