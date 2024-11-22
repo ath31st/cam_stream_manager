0bc2606e0d9016b2c12e9480720cd95a56b9c085
ath31st <ath31st@yandex.ru>
2024-11-22 10:53:45 +0300

    update and clean use video player

--- client/src/shared/video.player/api/use.stream.player.ts
+++ client/src/shared/video.player/api/use.stream.player.ts
@@ -8,20 +8,17 @@ const useStreamPlayer = (url: string) => {
 
   useEffect(() => {
     setError(null);
-    
+
     const isHls = url.endsWith('.m3u8');
     const isFlv = url.endsWith('.flv');
     const isTs = url.endsWith('.ts');
-    const isMp4 = url.endsWith('.mp4');
-    const isH264 = url.includes('h264');
-    const isH265 = url.includes('h265');
 
     const handleError = (message: string) => {
       setError(message);
     };
 
     if (videoRef.current) {
-      if (Hls.isSupported() && isHls) {
+      if (Hls.isSupported() && (isHls || isTs)) {
         const hls = new Hls();
         hls.loadSource(url);
         hls.attachMedia(videoRef.current);
@@ -44,29 +41,6 @@ const useStreamPlayer = (url: string) => {
         return () => {
           flvPlayer.destroy();
         };
-      } else if (isTs) {
-        const hls = new Hls();
-        hls.loadSource(url);
-        hls.attachMedia(videoRef.current);
-        hls.on(Hls.Events.MANIFEST_PARSED, () => {
-          videoRef.current?.play();
-        });
-
-        return () => {
-          hls.destroy();
-        };
-      } else if (isMp4 || isH264 || isH265) {
-        if (
-          videoRef.current.canPlayType('video/mp4; codecs="h264"') ||
-          videoRef.current.canPlayType('video/mp4; codecs="hev1"')
-        ) {
-          videoRef.current.src = url;
-          videoRef.current.addEventListener('loadedmetadata', () => {
-            videoRef.current?.play();
-          });
-        } else {
-          handleError('Формат потока не поддерживается для H.264 или H.265.');
-        }
       } else {
         handleError('Формат потока не поддерживается');
       }
