71463128a2b5a1b14aecb41f9dcba520863c99d9
ath31st <ath31st@yandex.ru>
2025-02-01 12:38:16 +0300

    rework header style and animation

--- client/src/widgets/header/ui/Header.tsx
+++ client/src/widgets/header/ui/Header.tsx
@@ -8,19 +8,43 @@ const { Header } = Layout;
 export const AppHeader: React.FC = () => {
   return (
     <Header className={styles.header}>
-      <div className={`${styles.waveWrapper} ${styles.waveAnimation}`}>
-        <div className={`${styles.waveWrapperInner} ${styles.bgTop}`}>
-          <div className={`${styles.wave} ${styles.waveTop}`} />
-        </div>
-        <div className={`${styles.waveWrapperInner} ${styles.bgMiddle}`}>
-          <div className={`${styles.wave} ${styles.waveMiddle}`} />
-        </div>
-        <div className={`${styles.waveWrapperInner} ${styles.bgBottom}`}>
-          <div className={`${styles.wave} ${styles.waveBottom}`} />
-        </div>
-      </div>
       <div className={styles.authModule}>
         <AuthModule />
+      </div>
+
+      <div className={styles.contentWrapper}>
+        <div className={styles.binaryAnimation}>
+          <div className={styles.binaryColumn}>
+            <span>01010110</span>
+            <span>11001011</span>
+            <span>00110101</span>
+            <span>10101010</span>
+          </div>
+          <div className={styles.binaryColumn}>
+            <span>11100011</span>
+            <span>00110011</span>
+            <span>10101010</span>
+            <span>01010101</span>
+          </div>
+          <div className={styles.binaryColumn}>
+            <span>00011100</span>
+            <span>11001100</span>
+            <span>01010101</span>
+            <span>11110000</span>
+          </div>
+          <div className={styles.binaryColumn}>
+            <span>10100101</span>
+            <span>01011010</span>
+            <span>11001100</span>
+            <span>00110011</span>
+          </div>
+          <div className={styles.binaryColumn}>
+            <span>11110000</span>
+            <span>00001111</span>
+            <span>10101010</span>
+            <span>01010101</span>
+          </div>
+        </div>
       </div>
     </Header>
   );
