0880dbee750e81e39690ed152c80966af32f6cff
ath31st <ath31st@yandex.ru>
2025-02-09 11:54:57 +0300

    add group repository

---
+++ server/src/repositories/group.repository.ts
@@ -0,0 +1,49 @@
+import { Group, PrismaClient } from '@prisma/client';
+import { Logger } from '../utils/logger';
+import { NewGroupDto } from '@shared/types';
+
+export class GroupRepository {
+  private prismaClient: PrismaClient;
+
+  constructor(prismaClient: PrismaClient) {
+    this.prismaClient = prismaClient;
+  }
+
+  findGroup = async (id: number): Promise<Group> => {
+    return await this.prismaClient.group.findUniqueOrThrow({
+      where: { id: id },
+    });
+  };
+
+  findGroups = async (): Promise<Group[]> => {
+    return await this.prismaClient.group.findMany();
+  };
+
+  createGroup = async (dto: NewGroupDto): Promise<Group> => {
+    const group = await this.prismaClient.group.create({
+      data: {
+        name: dto.name,
+      },
+    });
+    Logger.log(group);
+    return group;
+  };
+
+  updateGroup = async (id: number, dto: NewGroupDto): Promise<Group> => {
+    const group = await this.prismaClient.group.update({
+      where: { id },
+      data: {
+        name: dto.name,
+      },
+    });
+    Logger.log('Updated group', group);
+    return group;
+  };
+
+  deleteGroup = async (id: number) => {
+    await this.prismaClient.group.delete({
+      where: { id },
+    });
+    Logger.log(`Deleted group with id ${id}`);
+  };
+}
