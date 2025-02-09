22131180d94861ffe16bb224f44b59e15331850e
ath31st <ath31st@yandex.ru>
2025-02-09 12:41:31 +0300

    add import common types group for client

---
+++ client/src/entities/group/index.ts
@@ -0,0 +1,5 @@
+import { GroupDto, NewGroupDto, UpdateGroupDto } from "@shared/types";
+
+export type Group = GroupDto;
+export type NewGroup = NewGroupDto;
+export type UpdateGroup = UpdateGroupDto;
