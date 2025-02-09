b7f4a8e8ea4a2fc0e74505dcfb19a94b5cc49856
ath31st <ath31st@yandex.ru>
2025-02-09 12:27:16 +0300

    update group service with mapper. replace returning entity to dto

--- server/src/services/group.service.ts
+++ server/src/services/group.service.ts
@@ -1,7 +1,8 @@
 import { Group } from '@prisma/client';
 import { GroupRepository } from '../repositories/group.repository';
 import { Logger } from '../utils/logger';
-import { NewGroupDto, UpdateGroupDto } from '@shared/types';
+import { GroupDto, NewGroupDto, UpdateGroupDto } from '@shared/types';
+import { toGroupDto, toGroupDtos } from '../mappers/group.mapper';
 
 export class GroupService {
   private groupRepository: GroupRepository;
@@ -10,7 +11,7 @@ export class GroupService {
     this.groupRepository = groupRepository;
   }
 
-  getGroup = async (id: number): Promise<Group> => {
+  private getGroup = async (id: number): Promise<Group> => {
     try {
       return await this.groupRepository.findGroup(id);
     } catch (error) {
@@ -19,22 +20,26 @@ export class GroupService {
     }
   };
 
-  getGroups = async (): Promise<Group[]> => {
-    return await this.groupRepository.findGroups();
+  getGroupDto = async (id: number): Promise<GroupDto> => {
+    return this.getGroup(id).then(toGroupDto);
   };
 
-  createGroup = async (dto: NewGroupDto): Promise<Group> => {
+  getGroupDtos = async (): Promise<GroupDto[]> => {
+    return await this.groupRepository.findGroups().then(toGroupDtos);
+  };
+
+  createGroup = async (dto: NewGroupDto): Promise<GroupDto> => {
     try {
-      return await this.groupRepository.createGroup(dto);
+      return await this.groupRepository.createGroup(dto).then(toGroupDto);
     } catch (error) {
       Logger.error('Error creating group:', error);
       throw new Error('Cannot create group');
     }
   };
 
-  updateGroup = async (id: number, dto: UpdateGroupDto): Promise<Group> => {
+  updateGroup = async (id: number, dto: UpdateGroupDto): Promise<GroupDto> => {
     try {
-      return await this.groupRepository.updateGroup(id, dto);
+      return await this.groupRepository.updateGroup(id, dto).then(toGroupDto);
     } catch (error) {
       Logger.error(`Error updating group with id ${id}:`, error);
       throw new Error('Cannot update group');
