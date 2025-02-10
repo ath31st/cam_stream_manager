3c88933b447b61d40e18d58aef1bc71ae7ed44f5
ath31st <ath31st@yandex.ru>
2025-02-10 09:57:05 +0300

    update group index.ts with methods and modals

--- client/src/entities/group/index.ts
+++ client/src/entities/group/index.ts
@@ -1,4 +1,17 @@
-import { GroupDto, NewGroupDto, UpdateGroupDto } from "@shared/types";
+import { GroupDto, NewGroupDto, UpdateGroupDto } from '@shared/types';
+import AddGroupModal from './ui/AddGroupModal';
+import UpdateGroupModal from './ui/UpdateGroupModal';
+import DeleteGroupModal from './ui/DeleteGroupModal';
+import {
+  fetchGroup,
+  fetchGroups,
+  createGroup,
+  updateGroup,
+  deleteGroup,
+} from './api/group.api';
+
+export { fetchGroup, fetchGroups, createGroup, updateGroup, deleteGroup };
+export { AddGroupModal, UpdateGroupModal, DeleteGroupModal };
 
 export type Group = GroupDto;
 export type NewGroup = NewGroupDto;
