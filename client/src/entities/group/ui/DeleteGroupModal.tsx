9fcc4103b35f5d73f4c711b2f4b0c2ad910c6e91
ath31st <ath31st@yandex.ru>
2025-02-10 09:56:31 +0300

    group modals | delete

---
+++ client/src/entities/group/ui/DeleteGroupModal.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Modal } from 'antd';
+import styles from '../../../shared/styles/CommonModalStyle.module.css';
+import FooterModal from '../../../shared/ui/buttons/FooterModal';
+
+interface DeleteGroupModalProps {
+  visible: boolean;
+  onConfirm: () => void;
+  onCancel: () => void;
+}
+
+const DeleteGroupModal: React.FC<DeleteGroupModalProps> = ({
+  visible,
+  onConfirm,
+  onCancel,
+}) => {
+  return (
+    <Modal
+      className={styles.modal}
+      title={<p className={styles['modal-title']}>Подтверждение удаления</p>}
+      open={visible}
+      onOk={onConfirm}
+      onCancel={onCancel}
+      footer={
+        <FooterModal onCancel={onCancel} onOk={onConfirm} okText="Удалить" />
+      }
+    >
+      <div className={styles['modal-body']}>
+        <p>Вы уверены, что хотите удалить этою группу?</p>
+      </div>
+    </Modal>
+  );
+};
+
+export default DeleteGroupModal;
