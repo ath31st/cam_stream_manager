511d0bfc0beacc1e0dc204545bfa5d7b65aad908
ath31st <ath31st@yandex.ru>
2025-02-12 06:37:07 +0300

    update region repository after extending region model

--- server/src/repositories/region.repository.ts
+++ server/src/repositories/region.repository.ts
@@ -1,6 +1,7 @@
-import { PrismaClient, Region } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { NewRegionDto, UpdateRegionDto } from '@shared/types';
 import { Logger } from '../utils/logger';
+import { RegionWithGroups } from '../types/extended.types';
 
 export class RegionRepository {
   private prismaClient: PrismaClient;
@@ -9,9 +10,10 @@ export class RegionRepository {
     this.prismaClient = prismaClient;
   }
 
-  findRegion = async (id: number): Promise<Region> => {
+  findRegion = async (id: number): Promise<RegionWithGroups> => {
     return await this.prismaClient.region.findUniqueOrThrow({
       where: { id: id },
+      include: { groups: true },
     });
   };
 
@@ -23,33 +25,50 @@ export class RegionRepository {
     return region.length > 0;
   };
 
-  findRegions = async (isVisible?: boolean): Promise<Region[]> => {
+  findRegions = async (isVisible?: boolean): Promise<RegionWithGroups[]> => {
     return await this.prismaClient.region.findMany({
       where: {
         ...(isVisible !== undefined && { isVisible: isVisible }),
       },
+      include: { groups: true },
     });
   };
 
-  createRegion = async (dto: NewRegionDto): Promise<Region> => {
+  createRegion = async (dto: NewRegionDto): Promise<RegionWithGroups> => {
     const region = await this.prismaClient.region.create({
       data: {
         name: dto.name,
         isVisible: true,
+        ...(dto.groupIds && {
+          groups: {
+            connect: dto.groupIds.map((id) => ({ id })),
+          },
+        }),
+      },
+      include: {
+        groups: true,
       },
     });
     Logger.log(region);
     return region;
   };
 
-  updateRegion = async (dto: UpdateRegionDto): Promise<Region> => {
+  updateRegion = async (dto: UpdateRegionDto): Promise<RegionWithGroups> => {
     const region = await this.prismaClient.region.update({
       where: { id: dto.id },
       data: {
         name: dto.name,
         isVisible: dto.isVisible,
+        ...(dto.groupIds !== undefined && {
+          groups: {
+            set: [],
+            connect: dto.groupIds.map((id) => ({ id })),
+          },
+        }),
       },
+      include: { groups: true },
     });
+
     Logger.log(region);
     return region;
   };
