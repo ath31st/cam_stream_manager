c5300a828164212715dd7d62d421089341c7be03
ath31st <ath31st@yandex.ru>
2025-02-14 15:15:50 +0300

    rework passport config - simplify user information for request. remove fetching user from db

--- server/src/config/passport.ts
+++ server/src/config/passport.ts
@@ -1,7 +1,5 @@
 import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
 import { PassportStatic } from 'passport';
-import { User } from '@prisma/client';
-import { userService } from '../utils/init';
 
 const jwtSecret = process.env.JWT_SECRET || 'secret';
 
@@ -13,17 +11,7 @@ export const configurePassport = (passport: PassportStatic) => {
 
   passport.use(
     new JwtStrategy(opts, async (jwt_payload, done) => {
-      try {
-        const user: User | null = await userService.getUser(jwt_payload.userId);
-
-        if (user) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-        }
-      } catch (error) {
-        return done(error, false);
-      }
+      return done(null, jwt_payload);
     }),
   );
 };
