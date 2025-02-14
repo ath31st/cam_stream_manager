9768cae94b699fe91a2150cc6f5f3cd708cfc893
ath31st <ath31st@yandex.ru>
2025-02-14 09:39:16 +0300

    server side | add extended type for user with groups

--- server/src/types/extended.types.ts
+++ server/src/types/extended.types.ts
@@ -3,3 +3,7 @@ import { Prisma } from '@prisma/client';
 export type RegionWithGroups = Prisma.RegionGetPayload<{
   include: { groups: true };
 }>;
+
+export type UserWithGroups = Prisma.UserGetPayload<{
+  include: { groups: true };
+}>;
