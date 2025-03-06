25b9fdb834c278e02671d3125e33336ad46f6b7e
ath31st <ath31st@yandex.ru>
2025-03-06 11:32:16 +0300

    add absent index.ts for shared layer

---
+++ client/src/shared/index.ts
@@ -0,0 +1,21 @@
+import FooterModal from './ui/buttons/FooterModal';
+import NarrowButton from './ui/buttons/NarrowButton';
+import WideButton from './ui/buttons/WideButton';
+import IconButton from './ui/buttons/IconButton';
+import ActionButtons from './ui/buttons/ActionButtons';
+import PlaylistSelect from './ui/selects/PlaylistSelect';
+import LargeLoader from './ui/loaders/LargeLoader';
+import MediumLoader from './ui/loaders/MediumLoader';
+import TabContainer from './ui/containers/TabContainer';
+
+export {
+  FooterModal,
+  NarrowButton,
+  WideButton,
+  IconButton,
+  ActionButtons,
+  PlaylistSelect,
+  LargeLoader,
+  MediumLoader,
+  TabContainer,
+};
