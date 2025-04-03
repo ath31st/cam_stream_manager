48b4a84642172a587747251c8b77ad1a566ce3a3
ath31st <ath31st@yandex.ru>
2025-04-03 18:34:22 +0300

    rework use stream player hook with cleaning streams when switch

--- client/src/shared/video.player/player.hooks/use.stream.player.ts
+++ client/src/shared/video.player/player.hooks/use.stream.player.ts
@@ -7,12 +7,36 @@ import { determineStreamFormat } from './stream.format.detector';
 const useStreamPlayer = (url: string) => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const playerRef = useRef<{
+    hls?: Hls;
+    flv?: flvjs.Player;
+    dash?: ReturnType<dashjs.MediaPlayerFactory['create']>;
+  }>({});
 
   useEffect(() => {
     setError(null);
 
     const handleError = (message: string) => {
       setError(message);
+    };
+
+    const cleanupPreviousPlayer = () => {
+      if (playerRef.current.hls) {
+        playerRef.current.hls.destroy();
+        playerRef.current.hls = undefined;
+      }
+      if (playerRef.current.flv) {
+        playerRef.current.flv.destroy();
+        playerRef.current.flv = undefined;
+      }
+      if (playerRef.current.dash) {
+        playerRef.current.dash.reset();
+        playerRef.current.dash = undefined;
+      }
+      if (videoRef.current) {
+        videoRef.current.pause();
+        videoRef.current.src = '';
+      }
     };
 
     const initializePlayer = async () => {
@@ -25,17 +49,17 @@ const useStreamPlayer = (url: string) => {
           case 'hls':
             if (Hls.isSupported()) {
               const hls = new Hls();
+              playerRef.current.hls = hls;
               hls.loadSource(url);
               hls.attachMedia(videoRef.current);
               hls.on(Hls.Events.MANIFEST_PARSED, () => {
-                videoRef.current?.play();
+                videoRef.current?.play().catch(() => {});
               });
-
-              return () => hls.destroy();
-            }
-            if (videoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+            } else if (
+              videoRef.current.canPlayType('application/vnd.apple.mpegurl')
+            ) {
               videoRef.current.src = url;
-              videoRef.current.play();
+              videoRef.current.play().catch(() => {});
             } else {
               handleError('HLS не поддерживается');
             }
@@ -44,27 +68,28 @@ const useStreamPlayer = (url: string) => {
           case 'flv':
             if (flvjs.isSupported()) {
               const flvPlayer = flvjs.createPlayer({ type: 'flv', url });
+              playerRef.current.flv = flvPlayer;
               flvPlayer.attachMediaElement(videoRef.current);
               flvPlayer.load();
               flvPlayer.play();
-
-              return () => flvPlayer.destroy();
+            } else {
+              handleError('FLV не поддерживается');
             }
-            handleError('FLV не поддерживается');
             break;
 
           case 'dash':
             if (dashjs.supportsMediaSource()) {
               const player = dashjs.MediaPlayer().create();
+              playerRef.current.dash = player;
               player.initialize(videoRef.current, url, true);
-              return () => player.reset();
+            } else {
+              handleError('DASH не поддерживается');
             }
-            handleError('DASH не поддерживается');
             break;
 
           case 'native':
             videoRef.current.src = url;
-            videoRef.current.play();
+            videoRef.current.play().catch(() => {});
             break;
 
           default:
@@ -76,6 +101,10 @@ const useStreamPlayer = (url: string) => {
     };
 
     initializePlayer();
+
+    return () => {
+      cleanupPreviousPlayer();
+    };
   }, [url]);
 
   return { videoRef, error };
