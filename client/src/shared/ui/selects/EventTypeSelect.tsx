8a398154a018a1be8f6f6d430cc53ba5b2fed3cf
ath31st <ath31st@yandex.ru>
2025-05-16 09:32:18 +0300

    add event type select component with styles

---
+++ client/src/shared/ui/selects/EventTypeSelect.tsx
@@ -0,0 +1,46 @@
+import { Select } from 'antd';
+import type React from 'react';
+import styles from './EventTypeSelect.module.css';
+import type { EventType } from '../../../entities/event';
+
+interface EventTypeSelectProps {
+  eventTypes: EventType[];
+  value?: EventType;
+  onChange?: (value: EventType | undefined) => void;
+  placeholder?: string;
+  id?: EventType;
+}
+
+const EventTypeSelect: React.FC<EventTypeSelectProps> = ({
+  eventTypes,
+  value,
+  onChange,
+  placeholder = 'Фильтр по типу события',
+  id,
+}) => {
+  return (
+    <Select
+      id={id}
+      className={styles['search-select-field']}
+      showSearch
+      allowClear
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      optionFilterProp="children"
+      filterOption={(input, option) =>
+        (option?.children as unknown as string)
+          .toLowerCase()
+          .includes(input.toLowerCase())
+      }
+    >
+      {eventTypes.map((eventType) => (
+        <Select.Option key={eventType} value={eventType}>
+          {eventType}
+        </Select.Option>
+      ))}
+    </Select>
+  );
+};
+
+export default EventTypeSelect;
