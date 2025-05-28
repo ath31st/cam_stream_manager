4b3723a6c5256aec693526d6156a06416c9eb678
ath31st <ath31st@yandex.ru>
2025-05-28 18:12:32 +0300

    extract handlers from auth module

---
+++ client/src/features/auth/model/use.auth.module.handlers.ts
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { useAuthStore } from './auth.store';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { resetStores } from '../../../app';
+import { usePlaylistStore } from '../../../entities/playlist';
+import {
+  errorNotification,
+  successNotification,
+} from '../../../shared/notifications';
+import routes from '../../../shared/routes';
+
+const useAuthModuleHandlers = () => {
+  const { isAuthenticated, login, user, logout, clearError } = useAuthStore();
+  const [isLoginModalVisible, setLoginModalVisible] = useState(false);
+  const [isUserCardModalVisible, setUserCardModalVisible] = useState(false);
+  const [isLogoutConfirmModalVisible, setLogoutConfirmModalVisible] =
+    useState(false);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const playlistStore = usePlaylistStore();
+
+  const showLoginModal = () => {
+    setLoginModalVisible(true);
+  };
+
+  const closeLoginModal = () => {
+    setLoginModalVisible(false);
+  };
+
+  const handleLogin = async (username: string, password: string) => {
+    clearError();
+
+    await login(username, password);
+    const currentError = useAuthStore.getState().error;
+    if (currentError) {
+      errorNotification('Неавторизованный доступ', clearError, currentError);
+    } else {
+      successNotification('Успешный вход', 'Вы вошли в систему');
+    }
+    await playlistStore.fetchAllPlaylists(true);
+    setLoginModalVisible(false);
+  };
+
+  const showLogoutConfirmModal = () => {
+    setLogoutConfirmModalVisible(true);
+  };
+
+  const closeLogoutConfirmModal = () => {
+    setLogoutConfirmModalVisible(false);
+  };
+
+  const handleLogout = async () => {
+    await logout();
+    resetStores();
+    await playlistStore.fetchAllPlaylists(true);
+    successNotification('Успешный выход', 'Вы вышли из системы');
+    setLogoutConfirmModalVisible(false);
+  };
+
+  const handleAdminPageNavigation = () => {
+    navigate(routes.ADMIN);
+  };
+
+  const handlePlayerPageNavigation = () => {
+    navigate(routes.HOME);
+  };
+
+  const showUserCardModal = () => {
+    setUserCardModalVisible(true);
+  };
+
+  const closeUserCardModal = () => {
+    setUserCardModalVisible(false);
+  };
+
+  return {
+    state: {
+      user,
+      isAuthenticated,
+      location,
+    },
+    actions: {
+      handleLogin,
+      handleLogout,
+      handleAdminPageNavigation,
+      handlePlayerPageNavigation,
+    },
+    modals: {
+      showLoginModal,
+      closeLoginModal,
+      showLogoutConfirmModal,
+      closeLogoutConfirmModal,
+      showUserCardModal,
+      closeUserCardModal,
+      isLoginModalVisible,
+      isUserCardModalVisible,
+      isLogoutConfirmModalVisible,
+    },
+  };
+};
+
+export default useAuthModuleHandlers;
--- client/src/features/auth/ui/AuthModule.tsx
+++ client/src/features/auth/ui/AuthModule.tsx
@@ -1,81 +1,34 @@
 import type React from 'react';
-import { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { resetStores } from '../../../app';
-import { usePlaylistStore } from '../../../entities/playlist';
-import {
-  errorNotification,
-  successNotification,
-} from '../../../shared/notifications';
-import routes from '../../../shared/routes';
-import { useAuthStore } from '../model/auth.store';
 import AuthButtons from './AuthButtons';
 import AuthModals from './AuthModals';
+import useAuthModuleHandlers from '../model/use.auth.module.handlers';
 
 const AuthModule: React.FC = () => {
-  const { isAuthenticated, login, user, logout, clearError } = useAuthStore();
-  const [isLoginModalVisible, setLoginModalVisible] = useState(false);
-  const [isUserCardModalVisible, setUserCardModalVisible] = useState(false);
-  const [isLogoutConfirmModalVisible, setLogoutConfirmModalVisible] =
-    useState(false);
-
-  const navigate = useNavigate();
-  const location = useLocation();
-  const playlistStore = usePlaylistStore();
-
-  const handleLogin = async (username: string, password: string) => {
-    clearError();
-
-    await login(username, password);
-    const currentError = useAuthStore.getState().error;
-    if (currentError) {
-      errorNotification('Неавторизованный доступ', clearError, currentError);
-    } else {
-      successNotification('Успешный вход', 'Вы вошли в систему');
-    }
-    await playlistStore.fetchAllPlaylists(true);
-    setLoginModalVisible(false);
-  };
-
-  const handleLogout = async () => {
-    await logout();
-    resetStores();
-    await playlistStore.fetchAllPlaylists(true);
-    successNotification('Успешный выход', 'Вы вышли из системы');
-    setLogoutConfirmModalVisible(false);
-  };
-
-  const handleAdminPageNavigation = () => {
-    navigate(routes.ADMIN);
-  };
-
-  const handlePlayerPageNavigation = () => {
-    navigate(routes.HOME);
-  };
+  const { actions, state, modals } = useAuthModuleHandlers();
 
   return (
     <>
       <AuthButtons
-        isAuthenticated={isAuthenticated}
-        userRole={user?.role}
-        username={user?.username}
-        onShowUserCardModal={() => setUserCardModalVisible(true)}
-        onLogin={() => setLoginModalVisible(true)}
-        onLogout={() => setLogoutConfirmModalVisible(true)}
-        onAdminPageNavigation={handleAdminPageNavigation}
-        onPlayerPageNavigation={handlePlayerPageNavigation}
-        currentPath={location.pathname}
+        isAuthenticated={state.isAuthenticated}
+        userRole={state.user?.role}
+        username={state.user?.username}
+        onShowUserCardModal={modals.showUserCardModal}
+        onLogin={modals.showLoginModal}
+        onLogout={modals.showLogoutConfirmModal}
+        onAdminPageNavigation={actions.handleAdminPageNavigation}
+        onPlayerPageNavigation={actions.handlePlayerPageNavigation}
+        currentPath={state.location.pathname}
       />
       <AuthModals
-        isLoginModalVisible={isLoginModalVisible}
-        isUserCardModalVisible={isUserCardModalVisible}
-        isLogoutConfirmModalVisible={isLogoutConfirmModalVisible}
-        onCloseLoginModal={() => setLoginModalVisible(false)}
-        onCloseUserCardModal={() => setUserCardModalVisible(false)}
-        onCloseLogoutConfirmModal={() => setLogoutConfirmModalVisible(false)}
-        user={user}
-        onLogin={handleLogin}
-        onConfirmLogout={handleLogout}
+        isLoginModalVisible={modals.isLoginModalVisible}
+        isUserCardModalVisible={modals.isUserCardModalVisible}
+        isLogoutConfirmModalVisible={modals.isLogoutConfirmModalVisible}
+        onCloseLoginModal={modals.closeLoginModal}
+        onCloseUserCardModal={modals.closeUserCardModal}
+        onCloseLogoutConfirmModal={modals.closeLogoutConfirmModal}
+        user={state.user}
+        onLogin={actions.handleLogin}
+        onConfirmLogout={actions.handleLogout}
       />
     </>
   );
