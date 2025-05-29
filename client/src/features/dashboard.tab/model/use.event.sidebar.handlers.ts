3808af5c011734ff351fe9e2e3ed6f1c7619c21b
ath31st <ath31st@yandex.ru>
2025-05-29 16:32:52 +0300

    extract event sidebar logic to handlers

---
+++ client/src/features/dashboard.tab/model/use.event.sidebar.handlers.ts
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { useEventStore } from '../../../entities/event';
+import { fetchProps, POLLING_INTERVAL } from '../lib/event.sidebar.constants';
+import { errorNotification } from '../../../shared/notifications';
+
+const useEventSidebarHandlers = (isActiveTab: boolean) => {
+  const { error, clearError, fetchSidebarEvents, sidebarEvents } =
+    useEventStore();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (isActiveTab) {
+      fetchSidebarEvents(fetchProps.page, fetchProps.pageSize).then(() => {
+        setIsLoading(false);
+      });
+
+      const intervalId = setInterval(() => {
+        fetchSidebarEvents(fetchProps.page, fetchProps.pageSize);
+      }, POLLING_INTERVAL);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [fetchSidebarEvents, isActiveTab]);
+
+  useEffect(() => {
+    if (error) {
+      errorNotification('Ошибка в работе с событиями', clearError, error);
+    }
+  }, [error, clearError]);
+
+  return {
+    state: {
+      error,
+      sidebarEvents,
+      isLoading,
+      pageSize: fetchProps.pageSize,
+    },
+  };
+};
+
+export default useEventSidebarHandlers;
--- client/src/features/dashboard.tab/ui/EventSidebar.tsx
+++ client/src/features/dashboard.tab/ui/EventSidebar.tsx
@@ -1,51 +1,26 @@
 import type React from 'react';
-import { useEffect, useState } from 'react';
-import { useEventStore } from '../../../entities/event';
-import { errorNotification } from '../../../shared/notifications';
 import { MediumLoader } from '../../../shared/ui';
 import styles from './EventSidebar.module.css';
-import { fetchProps, POLLING_INTERVAL } from '../lib/event.sidebar.constants';
 import EventCardList from './EventCardList';
+import useEventSidebarHandlers from '../model/use.event.sidebar.handlers';
 
 interface EventSidebarProps {
   isActiveTab: boolean;
 }
 
 const EventSidebar: React.FC<EventSidebarProps> = ({ isActiveTab }) => {
-  const { error, clearError, fetchSidebarEvents, sidebarEvents } =
-    useEventStore();
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (isActiveTab) {
-      fetchSidebarEvents(fetchProps.page, fetchProps.pageSize).then(() => {
-        setIsLoading(false);
-      });
-
-      const intervalId = setInterval(() => {
-        fetchSidebarEvents(fetchProps.page, fetchProps.pageSize);
-      }, POLLING_INTERVAL);
-
-      return () => clearInterval(intervalId);
-    }
-  }, [fetchSidebarEvents, isActiveTab]);
-
-  useEffect(() => {
-    if (error) {
-      errorNotification('Ошибка в работе с событиями', clearError, error);
-    }
-  }, [error, clearError]);
+  const { state } = useEventSidebarHandlers(isActiveTab);
 
   return (
     <>
-      {isLoading ? (
+      {state.isLoading ? (
         <MediumLoader />
       ) : (
         <>
           <p className={styles['event-sidebar-header']}>
-            {`${fetchProps.pageSize} последних события:`}
+            {`${state.pageSize} последних события:`}
           </p>
-          <EventCardList events={sidebarEvents} />
+          <EventCardList events={state.sidebarEvents} />
         </>
       )}
     </>
