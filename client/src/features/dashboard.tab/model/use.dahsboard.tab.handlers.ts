cee72caa3e8ac824ccc8171d3d2e9d8121368bfc
ath31st <ath31st@yandex.ru>
2025-05-29 16:23:08 +0300

    extract dashboard tab logic to handlers

---
+++ client/src/features/dashboard.tab/model/use.dahsboard.tab.handlers.ts
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import type { PlaylistInfo } from '../../../shared/api.types';
+import { fetchDashboardData } from '../../../entities/dashboard';
+import { STREAMS_UPDATE_INTERVAL } from '../lib/dashboard.constants';
+
+const useDashboardTabHandlers = (isActiveTab: boolean) => {
+  const [dashboardData, setDashboardData] = useState<PlaylistInfo[]>([]);
+  const [openPlaylist, setOpenPlaylist] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchDashboardData();
+        if (JSON.stringify(data) !== JSON.stringify(dashboardData)) {
+          setDashboardData(data);
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching dashboard data:', error);
+        setIsLoading(false);
+      }
+    };
+
+    if (isActiveTab) {
+      fetchData();
+
+      const intervalId = setInterval(() => {
+        fetchData();
+      }, STREAMS_UPDATE_INTERVAL);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [isActiveTab, dashboardData]);
+
+  const togglePlaylist = (playlistName: string) => {
+    setOpenPlaylist((prev) => (prev === playlistName ? null : playlistName));
+  };
+
+  const sortPlaylistsByName = (pl1: PlaylistInfo, pl2: PlaylistInfo) =>
+    pl1.playlistName.localeCompare(pl2.playlistName);
+
+  return {
+    actions: {
+      togglePlaylist,
+      sortPlaylistsByName,
+    },
+    state: {
+      dashboardData,
+      openPlaylist,
+      isLoading,
+    },
+  };
+};
+
+export default useDashboardTabHandlers;
--- client/src/features/dashboard.tab/ui/DashboardTab.tsx
+++ client/src/features/dashboard.tab/ui/DashboardTab.tsx
@@ -1,66 +1,32 @@
 import { Col, Row } from 'antd';
 import type React from 'react';
-import { useEffect, useState } from 'react';
-import { fetchDashboardData } from '../../../entities/dashboard';
-import type { PlaylistInfo } from '../../../shared/api.types';
 import { LargeLoader } from '../../../shared/ui';
-import { STREAMS_UPDATE_INTERVAL } from '../lib/dashboard.constants';
 import PlaylistCard from './PlaylistCard';
 import EventSidebar from './EventSidebar';
+import useDashboardTabHandlers from '../model/use.dahsboard.tab.handlers';
 
 interface DashboardProps {
   isActiveTab: boolean;
 }
 
 const Dashboard: React.FC<DashboardProps> = ({ isActiveTab }) => {
-  const [dashboardData, setDashboardData] = useState<PlaylistInfo[]>([]);
-  const [openPlaylist, setOpenPlaylist] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchDashboardData();
-        if (JSON.stringify(data) !== JSON.stringify(dashboardData)) {
-          setDashboardData(data);
-        }
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching dashboard data:', error);
-        setIsLoading(false);
-      }
-    };
-
-    if (isActiveTab) {
-      fetchData();
-
-      const intervalId = setInterval(() => {
-        fetchData();
-      }, STREAMS_UPDATE_INTERVAL);
-
-      return () => clearInterval(intervalId);
-    }
-  }, [isActiveTab, dashboardData]);
-
-  const togglePlaylist = (playlistName: string) => {
-    setOpenPlaylist((prev) => (prev === playlistName ? null : playlistName));
-  };
+  const { actions, state } = useDashboardTabHandlers(isActiveTab);
 
   return (
     <Row gutter={16}>
       <Col span={18}>
-        {isLoading ? (
+        {state.isLoading ? (
           <LargeLoader />
         ) : (
           <Row gutter={[16, 22]}>
-            {dashboardData
-              .sort((a, b) => a.playlistName.localeCompare(b.playlistName))
+            {state.dashboardData
+              .sort(actions.sortPlaylistsByName)
               .map((playlist) => (
                 <Col key={playlist.playlistName} xs={24} sm={12} lg={8}>
                   <PlaylistCard
                     {...playlist}
-                    isOpen={openPlaylist === playlist.playlistName}
-                    onToggle={togglePlaylist}
+                    isOpen={state.openPlaylist === playlist.playlistName}
+                    onToggle={actions.togglePlaylist}
                   />
                 </Col>
               ))}
