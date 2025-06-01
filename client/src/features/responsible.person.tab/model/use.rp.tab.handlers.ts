77f296f5de94b3631ee70720afc61a0bbd4b3869
ath31st <ath31st@yandex.ru>
2025-06-01 11:31:24 +0300

    extract rp tab logic to handlers

---
+++ client/src/features/responsible.person.tab/model/use.rp.tab.handlers.ts
@@ -0,0 +1,150 @@
+import { useState, useEffect } from 'react';
+import { useResponsiblePersonStore } from '../../../entities/responsible.person';
+import { useStreamStore } from '../../../entities/stream';
+import type {
+  ResponsiblePerson,
+  NewResponsiblePerson,
+  UpdateResponsiblePerson,
+} from '../../../shared/api.types';
+import {
+  errorNotification,
+  successNotification,
+} from '../../../shared/notifications';
+import { useFilterResponsiblePersons } from '../lib/use.filter.rp';
+
+const useRpTabHandlers = () => {
+  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
+  const [isUpdateModalVisible, setIsUpdateModalVisible] = useState(false);
+  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [selectedPerson, setSelectedPerson] =
+    useState<ResponsiblePerson | null>(null);
+  const [selectedStreamId, setSelectedStreamId] = useState<number | null>(null);
+
+  const {
+    responsiblePersons,
+    fetchResponsiblePersons,
+    addResponsiblePerson,
+    updateResponsiblePerson,
+    removeResponsiblePerson,
+    error,
+    clearError,
+    loading,
+  } = useResponsiblePersonStore();
+  const { streams, fetchAllStreams } = useStreamStore();
+
+  useEffect(() => {
+    if (error) {
+      errorNotification(
+        'Ошибка в работе с ответсвенными лицами',
+        clearError,
+        error,
+      );
+    }
+  }, [error, clearError]);
+
+  useEffect(() => {
+    fetchAllStreams();
+    fetchResponsiblePersons();
+  }, [fetchResponsiblePersons, fetchAllStreams]);
+
+  const showAddModal = () => {
+    setIsAddModalVisible(true);
+  };
+
+  const closeAddModal = () => {
+    setIsAddModalVisible(false);
+  };
+
+  const handleAddPerson = async (person: NewResponsiblePerson) => {
+    await addResponsiblePerson(person);
+    if (useResponsiblePersonStore.getState().error === null) {
+      successNotification(
+        'Ответсвенное лицо добавлено',
+        `Ответсвенное лицо "${person.name}" успешно добавлено.`,
+      );
+      setIsAddModalVisible(false);
+    }
+  };
+
+  const showUpdateModal = (person: ResponsiblePerson) => {
+    setSelectedPerson(person);
+    setIsUpdateModalVisible(true);
+  };
+
+  const closeUpdateModal = () => {
+    setSelectedPerson(null);
+    setIsUpdateModalVisible(false);
+  };
+
+  const handleUpdatePerson = async (person: UpdateResponsiblePerson) => {
+    if (selectedPerson) {
+      await updateResponsiblePerson(selectedPerson.id, person);
+      if (useResponsiblePersonStore.getState().error === null) {
+        successNotification(
+          'Ответсвенное лицо обновлено',
+          `Ответсвенное лицо "${person.name}" успешно обновлено.`,
+        );
+        setIsUpdateModalVisible(false);
+      }
+    }
+  };
+
+  const showDeleteModal = (person: ResponsiblePerson) => {
+    setSelectedPerson(person);
+    setIsDeleteModalVisible(true);
+  };
+
+  const closeDeleteModal = () => {
+    setSelectedPerson(null);
+    setIsDeleteModalVisible(false);
+  };
+
+  const handleDeletePerson = async () => {
+    if (selectedPerson !== null) {
+      await removeResponsiblePerson(selectedPerson.id);
+      if (useResponsiblePersonStore.getState().error === null) {
+        successNotification(
+          'Ответсвенное лицо удалено',
+          'Ответсвенное лицо успешно удалено.',
+        );
+      }
+      setIsDeleteModalVisible(false);
+    }
+  };
+
+  const filteredResponsiblePersons = useFilterResponsiblePersons(
+    selectedStreamId,
+    responsiblePersons,
+  );
+
+  return {
+    modals: {
+      isAddModalVisible,
+      showAddModal,
+      closeAddModal,
+      isUpdateModalVisible,
+      showUpdateModal,
+      closeUpdateModal,
+      isDeleteModalVisible,
+      showDeleteModal,
+      closeDeleteModal,
+    },
+    actions: {
+      handleAddPerson,
+      handleUpdatePerson,
+      handleDeletePerson,
+    },
+    state: {
+      streams,
+      loading,
+      filteredResponsiblePersons,
+      selectedPerson,
+      selectedStreamId,
+    },
+    setters: {
+      setSelectedStreamId,
+    },
+  };
+};
+
+export default useRpTabHandlers;
--- client/src/features/responsible.person.tab/ui/ResponsiblePersonsTab.tsx
+++ client/src/features/responsible.person.tab/ui/ResponsiblePersonsTab.tsx
@@ -1,149 +1,53 @@
 import { Space } from 'antd';
 import type React from 'react';
-import { useEffect, useState } from 'react';
-import { useResponsiblePersonStore } from '../../../entities/responsible.person';
-import { useStreamStore } from '../../../entities/stream';
-import type {
-  NewResponsiblePerson,
-  ResponsiblePerson,
-  UpdateResponsiblePerson,
-} from '../../../shared/api.types';
-import {
-  errorNotification,
-  successNotification,
-} from '../../../shared/notifications';
 import {
   LargeLoader,
   StreamSelect,
   TabContainer,
   WideButton,
 } from '../../../shared/ui';
-import { useFilterResponsiblePersons } from '../lib/use.filter.rp';
 import ResponsiblePersonModals from './ResponsiblePersonModals';
 import ResponsiblePersonsTable from './ResponsiblePersonsTable';
+import useRpTabHandlers from '../model/use.rp.tab.handlers';
 
 const ResponsiblePersonsTab: React.FC = () => {
-  const [isAddModalVisible, setIsAddModalVisible] = useState(false);
-  const [isUpdateModalVisible, setIsUpdateModalVisible] = useState(false);
-  const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
-  const [selectedPerson, setSelectedPerson] =
-    useState<ResponsiblePerson | null>(null);
-  const [selectedStreamId, setSelectedStreamId] = useState<number | null>(null);
-
-  const {
-    responsiblePersons,
-    fetchResponsiblePersons,
-    addResponsiblePerson,
-    updateResponsiblePerson,
-    removeResponsiblePerson,
-    error,
-    clearError,
-    loading,
-  } = useResponsiblePersonStore();
-  const { streams, fetchAllStreams } = useStreamStore();
-
-  useEffect(() => {
-    if (error) {
-      errorNotification(
-        'Ошибка в работе с ответсвенными лицами',
-        clearError,
-        error,
-      );
-    }
-  }, [error, clearError]);
-
-  useEffect(() => {
-    fetchAllStreams();
-    fetchResponsiblePersons();
-  }, [fetchResponsiblePersons, fetchAllStreams]);
-
-  const handleAddPerson = () => {
-    setIsAddModalVisible(true);
-  };
-
-  const handleSavePerson = async (person: NewResponsiblePerson) => {
-    await addResponsiblePerson(person);
-    if (useResponsiblePersonStore.getState().error === null) {
-      successNotification(
-        'Ответсвенное лицо добавлено',
-        `Ответсвенное лицо "${person.name}" успешно добавлено.`,
-      );
-      setIsAddModalVisible(false);
-    }
-  };
-
-  const handleUpdatePerson = async (person: UpdateResponsiblePerson) => {
-    if (selectedPerson) {
-      await updateResponsiblePerson(selectedPerson.id, person);
-      if (useResponsiblePersonStore.getState().error === null) {
-        successNotification(
-          'Ответсвенное лицо обновлено',
-          `Ответсвенное лицо "${person.name}" успешно обновлено.`,
-        );
-        setIsUpdateModalVisible(false);
-      }
-    }
-  };
-
-  const handleDeletePerson = async () => {
-    if (selectedPerson !== null) {
-      await removeResponsiblePerson(selectedPerson.id);
-      if (useResponsiblePersonStore.getState().error === null) {
-        successNotification(
-          'Ответсвенное лицо удалено',
-          'Ответсвенное лицо успешно удалено.',
-        );
-      }
-      setIsDeleteModalVisible(false);
-    }
-  };
-
-  const filteredResponsiblePersons = useFilterResponsiblePersons(
-    selectedStreamId,
-    responsiblePersons,
-  );
+  const { actions, state, modals, setters } = useRpTabHandlers();
 
   return (
     <TabContainer>
       <Space>
-        <WideButton onClick={handleAddPerson}>
+        <WideButton onClick={modals.showAddModal}>
           Добавить ответственное лицо
         </WideButton>
         <StreamSelect
-          streams={streams}
-          onChange={(value) => setSelectedStreamId(value)}
+          streams={state.streams}
+          onChange={setters.setSelectedStreamId}
         />
       </Space>
 
-      {loading ? (
+      {state.loading ? (
         <LargeLoader />
       ) : (
         <ResponsiblePersonsTable
-          persons={filteredResponsiblePersons}
-          streams={streams}
-          onEdit={(person) => {
-            setIsUpdateModalVisible(true);
-            setSelectedPerson(person);
-          }}
-          onDelete={(person) => {
-            setIsDeleteModalVisible(true);
-            setSelectedPerson(person);
-          }}
+          persons={state.filteredResponsiblePersons}
+          streams={state.streams}
+          onEdit={modals.showUpdateModal}
+          onDelete={modals.showDeleteModal}
         />
       )}
 
       <ResponsiblePersonModals
-        isAddVisible={isAddModalVisible}
-        isUpdateVisible={isUpdateModalVisible}
-        isDeleteVisible={isDeleteModalVisible}
-        selectedPerson={selectedPerson}
-        streams={streams}
-        onAdd={handleSavePerson}
-        onUpdate={handleUpdatePerson}
-        onDelete={handleDeletePerson}
-        onCloseAdd={() => setIsAddModalVisible(false)}
-        onCloseUpdate={() => setIsUpdateModalVisible(false)}
-        onCloseDelete={() => setIsDeleteModalVisible(false)}
+        isAddVisible={modals.isAddModalVisible}
+        isUpdateVisible={modals.isUpdateModalVisible}
+        isDeleteVisible={modals.isDeleteModalVisible}
+        selectedPerson={state.selectedPerson}
+        streams={state.streams}
+        onAdd={actions.handleAddPerson}
+        onUpdate={actions.handleUpdatePerson}
+        onDelete={actions.handleDeletePerson}
+        onCloseAdd={modals.closeAddModal}
+        onCloseUpdate={modals.closeUpdateModal}
+        onCloseDelete={modals.closeDeleteModal}
       />
     </TabContainer>
   );
