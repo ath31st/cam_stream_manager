53052fe18df5bc0fcd79491e1176a9a1b917ae65
ath31st <ath31st@yandex.ru>
2025-06-07 21:21:12 +0300

    add 404 page files

---
+++ client/src/pages/404/ui/NotFoundPage.tsx
@@ -0,0 +1,27 @@
+import type React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { WideButton } from '../../../shared/ui';
+import styles from './AccessDeniedPage.module.css';
+
+const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
+  useEffect(() => {
+    document.title = 'Страница не найдена';
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>404</div>
+      <div className={styles.subtitle}>Извините, страница не найдена.</div>
+      <WideButton onClick={handleGoBack}>Вернуться назад</WideButton>
+    </div>
+  );
+};
+
+export default NotFoundPage;
