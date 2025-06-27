ccdd821e3d585a7aa86b6ba3147eb5444ea82088
ath31st <ath31st@yandex.ru>
2025-06-27 21:12:35 +0300

    change ../../ to @ for reset stores func

--- client/src/app/stores/resetStores.ts
+++ client/src/app/stores/resetStores.ts
@@ -1,9 +1,9 @@
-import { useEventStore } from '../../entities/event';
-import { useGroupStore } from '../../entities/group';
-import { usePlaylistStore } from '../../entities/playlist';
-import { useResponsiblePersonStore } from '../../entities/responsible.person';
-import { useStreamStore } from '../../entities/stream';
-import { useUserStore } from '../../entities/user';
+import { useEventStore } from '@/entities/event';
+import { useGroupStore } from '@/entities/group';
+import { usePlaylistStore } from '@/entities/playlist';
+import { useResponsiblePersonStore } from '@/entities/responsible.person';
+import { useStreamStore } from '@/entities/stream';
+import { useUserStore } from '@/entities/user';
 
 export const resetStores = () => {
   useEventStore.getState().resetStore();
